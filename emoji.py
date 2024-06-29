c0439625dd3f4374ed30dd258fd86a031bf104e0
prabin202121/Simple-Portfolio-Webpage. <sigdelprabin321@gmail.com>
2024-06-29 10:08:41 +0545

    First commit

---
+++ emoji.py
@@ -0,0 +1,5 @@
+#pip install emoji
+import emoji
+print(emoji.emojize("you look like an alien:alien:"))
+print(emoji.emojize("You are pure hearted person:red_heart:, you are kind with others.:hibiscus:"))
+print(emoji.emojize("I am confused:confused_face:"))
